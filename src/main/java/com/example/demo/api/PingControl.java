fc6c52e51765019bd2cc02e8639e57182be486bc
601198096@qq.com <601198096@qq.com>
2019-02-28 15:58:05 +0800

    init

---
+++ src/main/java/com/example/demo/api/PingControl.java
@@ -0,0 +1,22 @@
+package com.example.demo.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @version 1.0
+ * @Author: wanhao
+ * @Description
+ * @Date: Created in 15:50 2019/2/28
+ */
+@Controller
+@RequestMapping
+public class PingControl {
+
+    @RequestMapping("/ping")
+    @ResponseBody
+    public String ping(){
+        return "ping";
+    }
+}
